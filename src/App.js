6264fd882e79ebab19897b77f5542c35e091611a
Felipe Vargas <git@fvgs.ai>
2017-05-16 21:59:49 -0700

    Add normalize.css and use flexbox to center content

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import '../node_modules/normalize.css/normalize.css';
 import Intro from './Intro';
 
 export default class App extends Component {
+  startGame() {
+    console.log("Click");
+  }
+
   render() {
 		return (
 			<Intro handleClick={this.startGame}/>
 		);
-  }
-
-  startGame() {
-    console.log("Click");
   }
 }
--- src/Intro.js
+++ src/Intro.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import StartButton from './StartButton.js';
 
+const styles = {
+	width: '100vw',
+	height: '100vh',
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'center',
+	alignItems: 'center',
+}
+
 export default function Intro({handleClick}) {
 	return (
-		<div>
+		<div style={styles}>
 			<p>Air[tbd] Two truths and a lie</p>
 			<StartButton
 				onClick={() => handleClick()}
